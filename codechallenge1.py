951e6376f0b3bfbb1b8bfcf9bc5abaf15eda9547
RodriguezRafaelC <rafaelrodriguez1433@gmail.com>
2025-08-11 15:37:49 +0800

    Add files via upload

---
+++ codechallenge1.py
@@ -0,0 +1,3 @@
+name = input("Enter Your Name : ")
+
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\t\tHi\t\t\t\t*\n*\t\t\t\t     ",name,"\t\t\t\t\t*\n\t*\t\t\t\t\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
